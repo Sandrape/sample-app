fb61398ba01e96d717259c2c67e401ca5cdd635c
Sandrape <82sandrape@gmail.com>
2022-04-06 23:45:33 +0000

    Se cambio puerto 8080 al 5050

--- sample-app.sh
+++ sample-app.sh
@@ -8,15 +8,15 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "FROM python" >> tempdir/Dockerfile2
+echo "RUN pip install flask" >> tempdir/Dockerfile2
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile2
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile2
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile2
+echo "EXPOSE 5050" >> tempdir/Dockerfile2
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile2
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker run -t -d -p 5050:5050 --name samplerunning2 sampleapp
 docker ps -a 
